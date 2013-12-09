e10a35f63c83a09ee1c2dc696f4235aa8f2e6cf3
Jon Bolt <epic.bagel@gmail.com>
2013-12-09 08:13:56 +0000

    Track email field

--- custom_user/models/abstract_email_user.py
+++ custom_user/models/abstract_email_user.py
@@ -1,27 +1,25 @@
 from model_utils.models import TimeStampedModel
+from model_utils import FieldTracker
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import (
-	AbstractBaseUser, PermissionsMixin
-)
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
 from custom_user.managers import UserManager
 
 class AbstractEmailUser(TimeStampedModel, AbstractBaseUser, PermissionsMixin):
+	objects = UserManager()
+	USERNAME_FIELD = 'email'
+	# Track any changes to the email field by default. Useful when
+	# integrating with CRM systems
+	tracker = FieldTracker(fields=['email'])
 	email = models.EmailField(verbose_name='Email address', max_length = 255, unique = True, db_index = True)
 	# Base user stuff
 	is_active = models.BooleanField(default = True)
-	#is_superuser = models.BooleanField(default = False)
-	objects = UserManager()
-	USERNAME_FIELD = 'email'
-	def get_full_name(self):
-		# The user is identified by their email address
-		return self.email
-	def get_short_name(self):
-		# The user is identified by their email address
-		return self.email
+	# The user is identified by their email address
+	def get_full_name(self): return self.email
+	# The user is identified by their email address
+	def get_short_name(self): return self.email
 	# On Python 3: def __str__(self):
-	def __unicode__(self):
-		return self.email
+	def __unicode__(self): u"%s" % return self.email
 	@property
 	def is_staff(self):
 		return self.is_superuser
